14d40387df807f489356a805deb41f018dc82574
CREMA <crema@FAC1s-MacBookAir.local>
2020-12-03 10:16:04 +0900

    Complete CRUD

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
 	def index
-
+		@posts = Post.all.order("created_at DESC")
 	end
 
 	def show
@@ -8,11 +9,17 @@ class PostsController < ApplicationController
 	end
 
 	def new
-
+		@post = Post.new
 	end
 
 	def create
+		@post = Post.new(post_params)
 
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
 	end
 
 	def edit
@@ -20,11 +27,16 @@ class PostsController < ApplicationController
 	end
 
 	def update
-
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
 	end
 
 	def destroy
-
+		@post.destroy
+		redirect_to root_path
 	end
 
 	private 
